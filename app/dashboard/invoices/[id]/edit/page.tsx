936e9755f4257cbc768f4e8574f867c69afe840c
dcolc <dcolmenares@shokworks.io>
2024-01-14 18:47:39 -0600

    Added login

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from '@/app/ui/invoices/edit-form';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data'
+import { notFound } from 'next/navigation';
 
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id
@@ -9,17 +10,21 @@ export default async function Page({ params }: { params: { id: string } }) {
         fetchCustomers()
     ])
 
+    if (!invoice) {
+        notFound();
+    }
+
     return (
         <main>
             <Breadcrumbs
                 breadcrumbs={[
                     { label: 'Invoices', href: '/dashboard/invoices' },
                     {
-                      label: 'Edit Invoice',
-                      href: `/dashboard/invoices/${id}/edit`,
-                      active: true,
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true,
                     },
-                  ]}
+                ]}
             />
             <Form invoice={invoice} customers={customers} />
         </main>
